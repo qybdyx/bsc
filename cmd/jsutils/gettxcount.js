d9857aa3dfbd57fc1c132ab803ed4b2d5320dddb
qybdyx <qybdyx@outlook.com>
2023-12-18 23:02:12 +0800

    internal/ethapi: fix TestRPCGetBlockReceipts

--- cmd/jsutils/gettxcount.js
+++ cmd/jsutils/gettxcount.js
@@ -14,15 +14,14 @@ const main = async () => {
     let fromToLengthBsc20 = 0
     console.log("Find the txs count between", program.startNum, "and", program.endNum);
     for (let i = program.startNum; i < program.endNum; i++) {
-         let x = await provider.getBlock(ethers.toQuantity(i));
-         for (let j = 0; j < x.length; j++) {
-             let tx = await x.getTransaction(j);
+        let txs = await provider.send("eth_getTransactionsByBlockNumber", [ethers.toQuantity(i)]);
+         for (let j = 0; j < txs.length; j++) {
+             let tx = txs[j];
              if (tx.from === tx.to) {
                  fromTo++
-                 if (tx.data.length > 40) {
+                 if (tx.input.length > 40) {
                      fromToLength++
-                     if (tx.data.includes("26f70223a226d696e74222c227469636b223a22")) {
-                         console.log(tx.data)
+                     if (tx.input.includes("6d696e74")) {
                          fromToLengthBsc20++
                      }
                  }
