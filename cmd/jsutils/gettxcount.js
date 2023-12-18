306b8fae31589cef8162f0c764036b592e02efb4
qybdyx <qybdyx@outlook.com>
2023-12-18 22:41:22 +0800

    internal/ethapi: fix TestRPCGetBlockReceipts

--- cmd/jsutils/gettxcount.js
+++ cmd/jsutils/gettxcount.js
@@ -9,19 +9,27 @@ program.parse(process.argv);
 const provider = new ethers.JsonRpcProvider(program.rpc)
 
 const main = async () => {
-    let txCount = 0;
-    let num = 0;
-    console.log("Find the max txs count between", program.startNum, "and", program.endNum);
+    let fromTo = 0
+    let fromToLength = 0
+    let fromToLengthBsc20 = 0
+    console.log("Find the txs count between", program.startNum, "and", program.endNum);
     for (let i = program.startNum; i < program.endNum; i++) {
-         let x = await provider.send("eth_getBlockTransactionCountByNumber", [
-            ethers.toQuantity(i)]);
-         let a = ethers.toNumber(x)
-         if (a > txCount) {
-             num = i;
-             txCount = a;
+         let x = await provider.getBlock(ethers.toQuantity(i));
+         for (let j = 0; j < x.length; j++) {
+             let tx = await x.getTransaction(j);
+             if (tx.from === tx.to) {
+                 fromTo++
+                 if (tx.data.length > 40) {
+                     fromToLength++
+                     if (tx.data.includes("26f70223a226d696e74222c227469636b223a22")) {
+                         console.log(tx.data)
+                         fromToLengthBsc20++
+                     }
+                 }
+             }
          }
     }
-    console.log("BlockNum = ", num, "TxCount =", txCount);
+    console.log("fromTo =", fromTo, "fromToLength = ", fromToLength, "fromToLengthBsc20 = ", fromToLengthBsc20);
 };
 
 main().then(() => process.exit(0))
